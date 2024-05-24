07e4c67fe9f7beac794c195a84293efba237795f
JosephPDF <josephpf12@hotmail.com>
2024-05-24 15:09:12 -0600

    Inicializando challenge

---
+++ src/main/java/com/challengealura/LiteAlura/LiteAluraApplication.java
@@ -0,0 +1,13 @@
+package com.challengealura.LiteAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteAluraApplication.class, args);
+	}
+
+}
